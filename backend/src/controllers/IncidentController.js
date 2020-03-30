db0c2961b54f6bc3d5b60381beea6ca47390338c
Yamada, Marcia <marcia.yamada-ovidio@dxc.com>
2020-03-30 12:05:54 -0300

    semana omnistack 11

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,58 +1,60 @@
-const connection = require('../database/connection');
+const connection = require("../database/connection");
 
 module.exports = {
+  async index(req, res) {
+    const { page = 1 } = req.query;
 
-    async index(request, response){
-      const { page } = request.query;
+    const [count] = await connection("incidents").count();
 
-      const [count] = await connection('incidents').count();
+    const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.whatsapp",
+        "ongs.email",
+        "ongs.city",
+        "ongs.uf"
+      ]);
 
-      const incidents = await connection('incidents')
-        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-        .limit(5)
-        .offset((page -1) * 5)
-        .select(['incidents.*', 
-            'ongs.name', 
-            'ongs.email', 
-            'ongs.whatsapp', 
-            'ongs.city', 
-            'ongs.uf'
-        ]);
+    res.header("X-Total-Count", count["count(*)"]);
 
-      response.header('X-Total-Count', count['count(*)']);
+    return res.json(incidents);
+  },
 
-      return response.json(incidents);
+  async store(req, res) {
+    const { title, description, value } = req.body;
+    const ong_id = req.headers.authorization;
 
-    },
+    const [id] = await connection("incidents").insert({
+      title,
+      description,
+      value,
+      ong_id
+    });
 
-    async create(request, response){
-        const { title, description, value} = request.body;
-        const ong_id = request.headers.authorization;
-    
-        const [id] = await connection('incidents').insert({
-            title,
-            description,
-            value,
-            ong_id
-        })
-    
-        return response.json({ id })
-    },
+    return res.json({ id });
+  },
 
-    async delete(request, response){
-        const { id } = request.params;
-        const ong_id = request.headers.authorization;
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
 
-        const incident = await connection('incidents')
-            .where('id', id)
-            .select('ong_id')
-            .first();
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
 
-            if (incident.ong_id != ong_id){
-                return response.status(401).json({ error:'Operation not permitted'});
-            }
-
-            await connection('incidents').where('id', id).delete();
-            return response.status(204).send();
+    if (incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: "Operation not permitted" });
     }
-}
+
+    await connection("incidents")
+      .where("id", id)
+      .delete();
+
+    return res.status(204).send();
+  }
+};
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,29 +1,27 @@
-
-const crypto = require('crypto');
-const connection = require('../database/connection');
+const connection = require("../database/connection");
+const crypto = require("crypto");
 
 module.exports = {
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
 
-    async index(request, response){
-      const ongs = await connection('ongs').select('*');
-      return response.json(ongs);
+    return res.json(ongs);
+  },
 
-    },
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
 
-    async create(request, response){
-        const { name, email, whatsapp, city, uf } = request.body;
-    
-        const id = crypto.randomBytes(4).toString('HEX');
-    
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf,
-        })
-    
-        return response.json({ id })
-    }
-}
+    const id = crypto.randomBytes(4).toString("HEX");
+
+    await connection("ongs").insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return res.json({ id });
+  }
+};
